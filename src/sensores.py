d2657de4341abdc12a1439304ee2b30ce7c9e6d9
Andreza Nascimento <andrezacosta110@gmail.com>
2022-01-20 20:37:43 -0300

    Correção print

--- src/sensores.py
+++ src/sensores.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
                 #client.publish('v1/db5a8060-3e2b-11ec-8da3-474359af83d7/things/c269e860-5ef2-11ec-8da3-474359af83d7/data/0', ambiente.temperature)
                 print("Umidade: %-3.1f %%" % ambiente.humidity)
                 #client.publish('v1/db5a8060-3e2b-11ec-8da3-474359af83d7/things/c269e860-5ef2-11ec-8da3-474359af83d7/data/1', ambiente.humidity)
-                print("----------  {:>5}\t{:>5}".format("Saturacao", "Voltagem\n"))
+                print("----------  {:>5}\t{:>5}".format("Saturacao", "Voltagem"))
                 print("Sensor Solo: " + "{:>5}%\t{:>5.3f}\n".format(percent_translation(canal.value), canal.voltage))
         except Exception as error:
             raise error
